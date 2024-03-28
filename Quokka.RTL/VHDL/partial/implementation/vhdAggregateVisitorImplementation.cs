4f22cdbdebb5c4693b1e5708ad5a99be4fa9e7dc
Evgeny Muryshkin <evmuryshkin@gmail.com>
2024-03-28 20:49:44 +1100

    aggregates alignment, item per line

--- Quokka.RTL/VHDL/partial/implementation/vhdAggregateVisitorImplementation.cs
+++ Quokka.RTL/VHDL/partial/implementation/vhdAggregateVisitorImplementation.cs
@@ -7,7 +7,25 @@ namespace Quokka.RTL.VHDL.Implementation
 	{
 		public override void OnVisit(vhdAggregate obj)
 		{
-			_builder.Append($"({obj.Children.Select(Raw).StringJoin(", ")})");
+            var sources = obj.Children.Select(Raw).ToList();
+			_builder.AppendLine("(");
+            using (_builder.Indent())
+            {
+                for (var idx = 0; idx < sources.Count; idx++)
+                {
+                    _builder.AppendIndented(sources[idx]);
+                    if (idx == sources.Count - 1)
+                    {
+                        _builder.AppendUnindentedLine("");
+                    }
+                    else
+                    {
+                        _builder.AppendUnindentedLine(",");
+                    }
+                }
+            }
+
+            _builder.AppendLine(")");
 		}
 	}
 } // Quokka.RTL.VHDL
--- Quokka.RTL/Verilog/partial/implementation/vlgAggregateExpressionVisitorImplementation.cs
+++ Quokka.RTL/Verilog/partial/implementation/vlgAggregateExpressionVisitorImplementation.cs
@@ -7,7 +7,24 @@ namespace Quokka.RTL.Verilog.Implementation
 	{
 		public override void OnVisit(vlgAggregateExpression obj)
 		{
-			_builder.Append($"{{ {obj.Expressions.Select(Raw).ToCSV()} }}");
+			var sources = obj.Expressions.Select(Raw).ToList();
+            _builder.AppendLine($"{{");
+            using (_builder.Indent())
+			{
+				for (var idx = 0; idx < sources.Count; idx++) 
+				{
+					_builder.AppendIndented(sources[idx]);
+					if (idx == sources.Count - 1)
+					{
+						_builder.AppendUnindentedLine("");
+					}
+					else
+					{
+                        _builder.AppendUnindentedLine(",");
+                    }
+                }
+            }
+            _builder.AppendLine($"}}");
 		}
 	}
 }
